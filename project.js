082eea12531ea1cab6e973116a38536408908b8a
rcmehedi15 <hmdmehedi2016@gmail.com>
2023-03-06 20:51:30 +0600

    done

--- project.js
+++ project.js
@@ -1,4 +1,8 @@
+// Variable Declaration 
+
 var showClock ;
+
+// start button 
 function startBtn(){
     showClock = setInterval(starClock,500);
 }
@@ -6,14 +10,13 @@ function starClock(){
     document.getElementById('showValue').innerText = new Date().toLocaleTimeString();
 
 }
-// stop 
+// stop button 
 function stopBtn(){
     clearInterval(showClock);
 }
-// reset 
+// reset button 
 function resetBtn(){
     clearInterval(showClock);
     document.getElementById('showValue').innerText = "0:00:00 PM";
-
 
 }
